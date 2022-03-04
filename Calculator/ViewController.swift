4bae2a051773b641fe8be317740c9e5ee4d47013
LaptevaKate <ekaterina213@yandex.ru>
2022-03-04 20:28:54 +0300

    first commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Екатерина Лаптева on 4.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
